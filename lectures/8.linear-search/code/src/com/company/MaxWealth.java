8b4e17c19af34380ab5e831bd572d4dedae9fc6a
Preetham <varanasipreetham999@gmail.com>
2022-04-27 19:06:21 +0530

    Linear serach and binary search theory code and notes are included

---
+++ lectures/8.linear-search/code/src/com/company/MaxWealth.java
@@ -0,0 +1,30 @@
+package com.company;
+// https://leetcode.com/problems/richest-customer-wealth/
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {
+                {1,2,3},
+                {3,2,1}
+        };
+        System.out.println(maximumWealth(accounts));
+
+    }
+    static int maximumWealth(int[][] accounts) {
+        // person = rol
+        // account = col
+        int ans = Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            // when you start a new row, take a new sum for that row
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            // now we have sum of accounts of person
+            // check with overall ans
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
---
+++ lectures/9.binary-search/code/src/com/company/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = {2,4,6,9,11,12,14,20,36,48};
+        int target = 45;
+        System.out.println(binarySearch(arr,target));
+    }
+    static int binarySearch(int[] arr,int target){
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = start + (end - start) / 2;
+
+
+        while (start <= end){
+            mid =  start + (end - start) / 2;
+            if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else {
+                return arr[mid];
+            }
+        }
+        return arr[start];
+    }
+}
